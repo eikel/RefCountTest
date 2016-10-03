d45dd4967924f0922ff01512540d87087b6bfdfc
Benjamin Eikel <benjamin@eikel.org>
2016-10-03 08:29:34 +0200

    Add license header

--- Util.cpp
+++ Util.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2016 Benjamin Eikel <benjamin@eikel.org>
+ *
+ * This file is subject to the terms of the Mozilla Public License, v. 2.0.
+ * You should have received a copy of the MPL along with this library; see the
+ * file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 #include "Util/References.h"
 #include "Util/ReferenceCounter.h"
 #include <cstdint>
